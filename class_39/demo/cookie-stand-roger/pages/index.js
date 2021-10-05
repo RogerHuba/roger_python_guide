557fefa7745036c0d0c63440c68280ef52d0f025
Roger Huba <rogerhuba@gmail.com>
2021-10-05 00:16:52 -0500

    add class 39 demo

---
+++ class_39/demo/cookie-stand-roger/pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import { useAuth} from '../contexts/auth'
+
+export default function Home() {
+  
+  const { user, login } = useAuth();
+  
+  return (
+    <div>
+      <Head>
+        <title>Cookie Stand Demo</title>
+      </Head>
+      <main className="p-4 space-y-8 text-center">
+        <h1 className="text-4xl">Fetching Data from Authenticated API</h1>
+        <button onClick={() => login('admin', '123')} className="p-2 text-white bg-gray-500 rounded">Login</button>
+        {user ? (
+            <h2>Welcome {user.username}</h2>
+        ) : (
+            <h2>Need to log in</h2>
+        )}
+      </main>
+    </div>
+  )
+}
